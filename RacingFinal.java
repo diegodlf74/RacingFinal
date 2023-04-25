a721af14119a44fc203262c23185b655eca27e01
diegodlf74 <diegodlf74@gmail.com>
2023-04-25 13:12:30 -0700

    Update RacingFinal.java

--- RacingFinal.java
+++ RacingFinal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PizzasAndPeople {
+public class RacingFinal {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     //new
