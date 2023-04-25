362ce070673314d2792156ad02e51831af540085
Cucamongur <diablitos.maximus@gmail.com>
2023-04-25 13:28:45 -0700

    Update RacingFinal.java

--- RacingFinal.java
+++ RacingFinal.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class RacingFinal {
+  String pName = "";
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     //new comment here
+    System.out.println("Enter your name: ");
 
   }
 }
